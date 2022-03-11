05fd53079cdb3244e4aa63d50238befc3cd6c38a
parazitas36 <lukaslg96@gmail.com>
2022-03-11 22:44:48 +0200

    Sukurtas Modal langas

---
+++ components/blocks/AddPhotoBlock.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AddPhotoBlock = (props) => {
+  return (
+    <View>
+      <Text>AddPhotoBlock</Text>
+    </View>
+  )
+}
+
+export default AddPhotoBlock;
